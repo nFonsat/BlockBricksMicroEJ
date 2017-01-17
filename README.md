1f4fadbaae3bb61a249fd18764eb0ff4b5105365
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2017-01-17 23:06:02 +0100

    Add base project MicroEJ

---
+++ README.md
@@ -0,0 +1,23 @@
+<!--
+	Markdown
+-->
+
+# Overview
+MicroEJ Application: BlockBricksMicroEJ.
+
+## Usage
+Add the following line to your `module.ivy` or your `ivy.xml`:
+> `<dependency org="fr.fonsatnicolas.esgi" name="BlockBricksMicroEJ" rev="..."/>`
+
+# Requirements
+  - EDC-1.2 or higher
+  - Wadapps Framework 1.2.0 or higher
+
+## Dependencies
+_All dependencies are retrieved transitively by Ivy resolver_.
+
+# Source
+N/A
+
+## Restrictions
+None.
---
+++ src/main/java/fr/fonsatnicolas/esgi/BlockBricksActivity.java
@@ -0,0 +1,54 @@
+package fr.fonsatnicolas.esgi;
+
+import ej.microui.MicroUI;
+import ej.wadapps.app.Activity;
+
+public class BlockBricksActivity implements Activity {
+
+	@Override
+	public String getID() {
+		return "BlockBricksActivity";
+	}
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onRestart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart() {
+		MicroUI.start();
+	}
+
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onPause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
