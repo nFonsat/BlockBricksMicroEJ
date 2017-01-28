00195180a06764c997f7ea30a5be1873675cfa28
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2017-01-28 16:58:22 +0100

    Add new font size 04b

--- src/main/java/fr/fonsatnicolas/esgi/common/BBFonts.java
+++ src/main/java/fr/fonsatnicolas/esgi/common/BBFonts.java
@@ -3,5 +3,9 @@ package fr.fonsatnicolas.esgi.common;
 public class BBFonts {
 	
 	public static final int FONT_04B_15 = 81;
+	
+	public static final int FONT_04B_30 = 82;
+	
+	public static final int FONT_04B_50 = 83;
 
 }
