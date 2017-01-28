e859fc0eb565251dd4710b1b0c29e2dfec7b1f30
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2017-01-28 16:33:36 +0100

    Add log

--- src/main/java/fr/fonsatnicolas/esgi/common/BBFontLoader.java
+++ src/main/java/fr/fonsatnicolas/esgi/common/BBFontLoader.java
@@ -12,10 +12,13 @@ public class BBFontLoader extends AbstractFontLoader {
 	protected int getFontHeight(FontProfile fontProfile) {
 		switch(fontProfile.getSize()) {
 		case LENGTH:
+			System.out.println("use BBFontLoader_LENGTH");
 			return fontProfile.getSizeValue();
 		case LARGE:
+			System.out.println("use BBFontLoader_LARGE_HEIGHT");
 			return LARGE_HEIGHT;
 		case MEDIUM:
+			System.out.println("use BBFontLoader_MEDIUM");
 		default:
 			return MEDIUM_HEIGHT;
 		}
